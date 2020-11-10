2b5b8fb1836330615b07a36349c80067932619d2
MounirBlk <mounir.ballouk@gmail.com>
2020-11-10 10:02:33 +0100

    commit

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 const express = require('express'); // Chargement Express
 const app = express(); // Instance Express
+const route = require('./src/routes');
+
 
 const port = process.env.PORT || 8020; // Port ecoute du server
 const www = process.env.WWW || './public'; // Point racine pour le dossier public
+// Middelware
+app.use(express.static(www)); // Emplacement du dossier public dans express
 
-//midleware
-app.use(express.static(www)); // Emplacement du dossier public
+app.use(route);
 
 // Route par defautl - Importante
 app.get('*', (req, res) => {
+    // Verif user
     res.sendFile(`index.html`, { root: www });
 });
 
-// Run servee
+
+// Run serve
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
